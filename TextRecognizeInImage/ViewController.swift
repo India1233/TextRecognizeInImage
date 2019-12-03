5a79ebc5a802143eadb4dcc8e2473ffbc1a15f80
Sachin Prakash Dhumal <sachin.dumal@indigo.co.in>
2019-12-03 12:25:54 +0530

    Initial Commit

---
+++ TextRecognizeInImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognizeInImage
+//
+//  Created by Sachin Dumal on 03/12/19.
+//  Copyright © 2019 Sachin Dumal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
